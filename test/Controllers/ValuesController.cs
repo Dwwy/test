3f192603ff1bcc87c9f742387b4b3d53d8b228dc
Daniel Wong Wei Yang <daniel.wongwy@techbase.my>
2024-03-05 18:13:52 +0800

    [Test]

--- test/Controllers/ValuesController.cs
+++ test/Controllers/ValuesController.cs
@@ -77,7 +77,7 @@ namespace test.Controllers
             //@WALLETTYPE = ?;", "000001", "MY27749776", 78.0, "", 1);
             //        db.executeScalarSP(sbSQL);
             //        return true;
-            return "Hi, Daniel's Test Project";
+            return "Hi, Daniel's Test Project test#1";
         }
 
         // GET api/values/5
